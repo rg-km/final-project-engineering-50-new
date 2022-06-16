b38c016ebe87afc3c4b3b17344d14dfaa768943a
nadaakn <nadaa.khoirunnafisah@gmail.com>
2022-06-16 23:31:03 +0700

    init app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+
+import Navbar from './components/Navbar';
+
+import { Routes, Route } from 'react-router-dom';
+import Home from './components/home/Home';
+import AboutUs from './components/home/AboutUs';
+import Product from './components/home/Product';
+import Testimoni from './components/home/Testimoni';
 
 function App() {
   return (
+     
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+          <Routes>
+            <Route>
+              <Route path="/" element={<Home />} />
+              <Route path="/aboutus" element={<AboutUs />} />
+              <Route path="/product" element={<Product />} />
+              <Route path="/testimoni" element={<Testimoni />} />
+            </Route>
+          </Routes>
+   
+    
     </div>
+    
   );
 }
 
---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import './Navbar.css';
+import { Link } from 'react-router-dom';
+
+export default class Navbar extends Component { 
+    constructor() {
+        super();
+        this.state = {
+            show: true, 
+        };
+    }
+    render() {
+        return (
+            <nav className="navbar fixed-top navbar-expand-lg headerbg">
+            <div className="container">
+              <Link to='/'>
+              <img src='./logo.png' width='90'/>
+              </Link>
+              <button className="navbar-toggler border border-info text-info"
+              onClick={ () =>{ this.setState({show: !this.state.show}) } }>
+                <span className="navbar-toggler-icon"></span>
+              </button>
+              <div className={ this.state.show ? "collapse navbar-collapse" : "collapse navbar-collapse active" }>
+                <div className="navbar-nav ms-auto">
+                <li className="nav-item">
+                  <Link className="nav-link text-light" to='/' >Home</Link>
+                </li>
+                  <li className="nav-item">
+                  <Link className="nav-link text-light" to='/product' >Product</Link>
+                  </li>
+                  <li className="nav-item">
+                  <Link className="nav-link text-light" to='/testimoni' >Testimoni</Link>
+                  </li>
+                  <li className="nav-item">
+                  <Link className="nav-link text-light" to='/aboutus' >AboutUs</Link>
+                  </li>
+                  <li className="nav-item">
+                  <Link className="nav-link text-light" to='/signin' >Sign in</Link>
+                  </li>
+                </div>
+              </div>
+            </div>
+          </nav>
+        );
+    }
+}
+    
