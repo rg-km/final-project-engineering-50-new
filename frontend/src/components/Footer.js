2fcd6bd800250228b38b6e95403ad850362e7cc8
nadaakn <nadaa.khoirunnafisah@gmail.com>
2022-06-23 06:49:00 +0700

    layouting footer

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -4,18 +4,20 @@ import './Footer.css'
 
 function Footer() {
   return (
-        <footer id="footerbg" className="p-4">
-
+        <footer className='fixed-footer'>
+      
         <div className="container p-4">
             <div className="row">
-                <div className="col-sm-4">
-                  <Link to='/' className='col-12 col-md-3 d-flex aling-items-center justyfy-content-center'>
-                      <img src='./logo.png' width='200'/>
+                <div className="col-sm-4 mt-3">
+                  <Link to='/' className='logo'>
+                      <img src='./logo.png' width='150'/>
                       </Link>
+                      <p className="caption">"The ability make yourself<br /> completely unique"</p>
     </div>
     
     <ul className='col-12 col-md-3 list-unstyled'>
-      <li className="font-weight-bold"><h4>AIMPROVE</h4></li>
+      <li className="font-weight-bold text-dark"><h6>AIMPROVE</h6></li>
+    <div className='text-footer text-light'>
     <li>
     <Link className="text-decoration-none text-dark" to='/'>HOME</Link>
     </li>
@@ -31,24 +33,27 @@ function Footer() {
     <li>
     <Link  className="text-decoration-none text-dark" to='/aboutus'>ABOUT US</Link>
     </li>
+    </div>
     </ul>
     <ul className='col-12 col-md-3 list-unstyled'>
-      <li className="font-weight-bold"><h4>FIND US</h4> </li>
-      <li className='d-flex justify-content-between p-2 col-12 col-md-3'>
+      <li className="font-weight-bol text-dark"><h6>SOCIAL MEDIA</h6> </li>
+      <li className='d-flex justify-content-between '>
+        <div className='icons-footer btn btn-link btn-floating btn-lg text-dark m-1'>
         <i className='bi bi-linkedin'/>
-        <i className='bi bi-instagram'/>
-        <i className='bi bi-whatsapp'/>
-    
+        <i className='bi bi-instagram p-3'/>
+        <i className='bi bi-tiktok'/>
+        </div>
     </li>
     </ul>
             
     
-    <div className="text-center p-4 p-3 mb-2 bg ">
-    © 2021 Copyright: 
+    <div className="text-center p-4 p-3 mb-2 bg text-dark block-footer">
+    © 2022 Copyright: 
     <a className="text-decoration-none text-dark fw-bold">AIMPROVE</a>
   </div>
 </div>
 </div>
+
 
 </footer>
   )
